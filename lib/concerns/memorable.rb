da63768381ac52dd15b271ff1d7887e1c22b2501
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-31 16:55:57 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,6 +1,5 @@
 module Memorable 
   module ClassMethods
-    
     def reset_all
       self.all.clear
     end
@@ -8,4 +7,8 @@ module Memorable
     def count
       self.all.count
     end
+  module InstanceMethods
+    def initialize
+      self.class.all << self 
+    end  
 end  
